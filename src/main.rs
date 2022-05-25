0b49b9dbd6ced983c1f843597b69bf2ac47b1f70
François <mockersf@gmail.com>
2022-05-26 01:38:38 +0200

    use the canvas

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ fn main() {
     App::new()
         .insert_resource(WindowDescriptor {
             title: "Bevy Playground".to_string(),
+            #[cfg(target_arch = "wasm32")]
+            canvas: Some("#playground".to_string()),
             ..default()
         })
         .add_plugins(DefaultPlugins)
